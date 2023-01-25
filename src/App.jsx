3698b33df30e8b4ec97c65fa88dbcaefbdfd242c
MariaOliu <maryoliu1@hotmail.com>
2023-01-25 18:42:47 +0100

    Juego terminado con LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,71 @@
 import { useState } from "react";
-
-const TURN = {
-  X: "X",
-  O: "0",
-};
-
-const Square = ({ children, index, isSelected, updateBoard }) => {
-  const className = `square ${isSelected ? "is-selected" : ""}`;
-  const handleClick = () => {
-    updateBoard(index);
-  };
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  );
-};
-
-
-
-
-
+import confetti from "canvas-confetti";
+import Square from "./components/Square";
+import { TURN } from "./constants";
+import { checkEndGame, checkWinner } from "./logic/board";
+import WinnerModal from "./components/WinnerModal";
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURN.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURN.X;
+  });
+  const [winner, setWinner] = useState(null);
+  // null es que no hay ganador, false es que hay empate
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURN.X);
+    setWinner(null);
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+
+  };
 
   const updateBoard = (index) => {
     // si ya tiene algo no se actualiza esta posicion
-    if (board[index]) return;
+    if (board[index] || winner) return;
+
     // actualizamos el tablero
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
+
     // cambiamos el turno del jugador
     const changeTurn = turn === TURN.X ? TURN.O : TURN.X;
     setTurn(changeTurn);
+    //guardar partida en localstorage
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", changeTurn);
+
+    // revisar si hay winner
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false); //empate
+    }
+
+    // revisar si hay empate
   };
 
   return (
     <main className="board">
       <h1>Tic tac toc</h1>
+      <button onClick={resetGame}>Resetear el juego</button>
       <section className="game">
-        {board.map((_, index) => {
+        {board.map((square, index) => {
           return (
             <Square key={index} index={index} updateBoard={updateBoard}>
-              {board[index]}
+              {square}
             </Square>
           );
         })}
@@ -55,6 +74,8 @@ const App = () => {
         <Square isSelected={turn === TURN.X}>{TURN.X}</Square>
         <Square isSelected={turn === TURN.O}>{TURN.O}</Square>
       </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame} />
     </main>
   );
 };
